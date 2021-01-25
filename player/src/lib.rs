f852333dd26513643b0ca3df74291772a720731e
goldencm203 <goldencm203@potsdam.edu>
2021-01-25 13:12:18 -0500

    Deleted go_fish player lib needs refactoring

--- player/src/lib.rs
+++ player/src/lib.rs
@@ -1,7 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate cards_lib as lib;
+use lib::card::*;
+use lib::deck::*;
+
+#[derive(Debug)]
+pub struct Player {
+    pub player_id : u32,
+    pub  cards : Deck,
+    score : u32,
+    pub asked : Vec<Number>
+}
+
+impl Player {
+    
+    /*Creates a new player object with 
+        Player ID, Deck (Vec<Cards>), Number of cards to be inserted into deck 
+        The default score is set to 0 when making a new player*/
+    pub fn new(player_id : u32, deck : &mut Deck, n_cards : usize) -> Player { 
+        Player{player_id, cards : Deck((deck.deal_n(n_cards)).to_vec()), score : 0, asked : Vec::new()}
+    }
+
+    pub fn get_score(&self) -> u32{
+        self.score
+    }
+
+    pub fn update_score(&mut self) {
+        self.score += 1;
+    }
+
+    pub fn display_hand(&self) {
+        println!("Player's {} hand", self.player_id);
+        println!("{}", self.cards);
     }
 }
